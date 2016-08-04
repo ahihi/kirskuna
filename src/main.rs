b1c4e7c462fa6d5ad4bd82bf73c145bc05625745
Miranda Kastemaa <miranda@foldplop.com>
2016-08-04 23:50:37 +0300

    Parameterize

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,122 @@
+#![feature(unboxed_closures)]
 extern crate dsp;
+extern crate getopts;
 extern crate portaudio;
 
 extern crate kirskuna;
 
+use std::cmp;
+use std::env;
+use std::error::Error;
+
 use dsp::{slice, Graph, Node, NodeIndex};
 use dsp::sample::ToFrameSliceMut;
+use getopts::Options;
 use portaudio as pa;
 
-use kirskuna::base::{Output, CHANNELS};
+use kirskuna::base::{Output};
 use kirskuna::clip_cubic::{ClipCubic};
 use kirskuna::delay::{Delay};
 use kirskuna::input::{Input};
 use kirskuna::node::{DspNode};
 //use kirskuna::sine::{Sine};
 
-const FRAMES: u32 = 64;
 const SAMPLE_HZ: f64 = 44_100.0;
 
-const IN_CHANNELS: usize = 6;
-const IN_LEFT: usize = 2;
-const IN_RIGHT: usize = 3;
-
-fn main() {
-    run().unwrap()
+struct RunOptions {
+    buf_size: usize,
+    in_channels: usize,
+    in_left: usize,
+    in_right: usize,
+    out_channels: usize,
+    out_left: usize,
+    out_right: usize
 }
 
-fn run() -> Result<(), pa::Error> {
+enum Command {
+    Help(String),
+    Run(RunOptions)
+}
+
+fn main() {
+    match get_command().unwrap() {
+        Command::Help(text)     => println!("{}", text),
+        Command::Run(run_opts)  => run(&run_opts).unwrap()
+    }
+}
+
+fn get_usage(program: &str, opts: Options) -> String {
+    let brief = format!("Usage: {} [options]", program);
+    format!("{}", opts.usage(&brief))
+}
+
+fn get_command() -> Result<Command, Box<Error>> {
+    let mut opts = Options::new();
+    
+    opts.optflag("h", "help", "print help");
+    opts.optopt("b", "buf-size", "buffer size", "FRAMES");
+    opts.optopt("l", "in-left", "left input channel", "CHANNEL");
+    opts.optopt("r", "in-right", "right input channel", "CHANNEL");
+    opts.optopt("L", "out-left", "left output channel", "CHANNEL");
+    opts.optopt("R", "out-right", "right output channel", "CHANNEL");
+        
+    let args: Vec<String> = env::args().collect();
+    
+    let m = try!(opts.parse(&args));
+    
+    if m.opt_present("h") {
+        return Ok(Command::Help(get_usage(&args[0], opts)));
+    }
+        
+    let buf_size = match m.opt_str("b") {
+        Some(s) => try!(s.parse::<usize>()),
+        None    => 64
+    };
+    
+    let in_left = match m.opt_str("l") {
+        Some(s) => try!(s.parse::<usize>()),
+        None    => 0
+    };
+
+    let in_right = match m.opt_str("r") {
+        Some(s) => try!(s.parse::<usize>()),
+        None    => 1
+    };
+    
+    let in_channels = cmp::max(in_left, in_right) + 1;
+
+    let out_left = match m.opt_str("L") {
+        Some(s) => try!(s.parse::<usize>()),
+        None    => 0
+    };
+
+    let out_right = match m.opt_str("R") {
+        Some(s) => try!(s.parse::<usize>()),
+        None    => 1
+    };
+
+    let out_channels = cmp::max(out_left, out_right) + 1;
+
+    Ok(Command::Run(RunOptions {
+        buf_size: buf_size,
+        in_channels: in_channels,
+        in_left: in_left,
+        in_right: in_right,
+        out_channels: out_channels,
+        out_left: out_left,
+        out_right: out_right
+    }))
+}
+
+fn run(opts: &RunOptions) -> Result<(), pa::Error> {    
+    let buf_size = opts.buf_size;
+    let in_left = opts.in_left;
+    let in_right = opts.in_right;
+    let in_channels = opts.in_channels;
+    let out_left = opts.out_left;
+    let out_right = opts.out_right;
+    let out_channels = opts.out_channels;
+    
     let mut graph = Graph::new();
 
     let master = graph.add_node(DspNode::Blank);
@@ -48,7 +139,7 @@ fn run() -> Result<(), pa::Error> {
     );*/
     
     let (_, input) = graph.add_input(
-        DspNode::Input(Input::new(FRAMES as usize)),
+        DspNode::Input(Input::new(buf_size)),
         clip_cubic
     );
     inputs.push(input);
@@ -61,12 +152,12 @@ fn run() -> Result<(), pa::Error> {
     let callback = move |pa::stream::DuplexCallbackArgs { in_buffer, out_buffer, time, .. }| {
         for &input_ix in &inputs {
             if let Some(&mut DspNode::Input(ref mut input_node)) = graph.node_mut(input_ix) {
-                let frames = in_buffer.chunks(IN_CHANNELS).map(|frame| [frame[IN_LEFT], frame[IN_RIGHT]]);
+                let frames = in_buffer.chunks(in_channels).map(|frame| [frame[in_left], frame[in_right]]);
                 input_node.fill(frames);
             }
         }
         
-        let out_buffer: &mut [[Output; CHANNELS]] = out_buffer.to_frame_slice_mut().unwrap();
+        let out_buffer: &mut [[Output; 2]] = out_buffer.to_frame_slice_mut().unwrap();
         slice::equilibrium(out_buffer);
         graph.audio_requested(out_buffer, SAMPLE_HZ);
 
@@ -80,7 +171,7 @@ fn run() -> Result<(), pa::Error> {
 
     let pa = try!(pa::PortAudio::new());
         
-    let settings = try!(pa.default_duplex_stream_settings::<Output, Output>(IN_CHANNELS as i32, CHANNELS as i32, SAMPLE_HZ, FRAMES));
+    let settings = try!(pa.default_duplex_stream_settings::<Output, Output>(in_channels as i32, out_channels as i32, SAMPLE_HZ, buf_size as u32));
     
     let mut stream = try!(pa.open_non_blocking_stream(settings, callback));
     try!(stream.start());
